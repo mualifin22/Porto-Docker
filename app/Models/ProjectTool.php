e9f1a667b8fb1e89aad3f3e21df586ce188ca5a4
mualifin22 <mimualifin22@gmail.com>
2025-03-06 10:36:55 +0700

    add softDeletes to projectTools

--- app/Models/ProjectTool.php
+++ app/Models/ProjectTool.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ProjectTool extends Model
 {
-    //
+       use HasFactory, SoftDeletes;
+
+    protected $guarded = [
+        'id'
+    ];
 }
--- database/migrations/2025_03_06_031309_create_project_tools_table.php
+++ database/migrations/2025_03_06_031309_create_project_tools_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('tool_id')->constrained()->cascadeOnDelete();
             $table->foreignId('project_id')->constrained()->cascadeOnDelete();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
